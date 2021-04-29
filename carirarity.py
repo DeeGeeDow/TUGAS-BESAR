268ebdc94d0bcd8a170fe95a66c0229e6bd66b7a
nrhshofi <82795941+nrhshofi@users.noreply.github.com>
2021-04-29 23:00:31 +0700

    Update carirarity.py

--- carirarity.py
+++ carirarity.py
@@ -14,10 +14,13 @@ def carirarity():
     for i in range (len(data_gadget)):
         if (data_gadget[i][4] == req):
             state = False
-            print("\nNama            :", data_gadget[i][1])
-            print("Deskripsi       :", data_gadget[i][2])
-            print("Jumlah          :", data_gadget[i][3])
-            print("Rarity          :", data_gadget[i][4])
-            print("Tahun Ditemukan :", data_gadget[i][5])
+            output(data_gadget, i)
     if state:
         print("\nTidak ditemukan gadget dengan rarity", req)
+
+def output(data, index):
+    print("\nNama            :", data[index][1])
+    print("Deskripsi       :", data[index][2])
+    print("Jumlah          :", data[index][3])
+    print("Rarity          :", data[index][4])
+    print("Tahun Ditemukan :", data[index][5])
